e1853077db995c694078db9592df77535b923180
Bernhard Maier <mail@neopheus.de>
2021-10-13 18:25:50 +0200

    feat(project): add initial solution and console project

---
+++ src/DummyDataGenerator.Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DummyDataGeneratorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Customer
+    {
+
+    }
+
+    public class Vehicle
+    {
+
+    }
+
+    public class Connection
+    {
+        
+    }
+
+    public interface IDummyDataGenerator
+    {
+        string GenerateRandomGuid();
+        int GenerateRandomGender();
+        string GenerateRandomDesignation();
+        string GenerateRandomFirstName();
+        string GenerateRandomLastName();
+        string GenerateRandomStreet();
+        string GenerateRandomHouseNumber();
+        string GenerateRandomZipCode();
+        string GenerateRandomCity();
+        string GenerateRandomPhone();
+        string GenerateRandomEmail();
+        string GenerateRandomManufacturer();
+        string GenerateRandomModel();
+        string GenerateRandomLicensePlate();
+        string GenerateRandomVin();
+        string GenerateRandomHsn();
+        string GenerateRandomTsn();
+        int GenerateRandomMilage();
+    }
+
+    public class DummyDataGenerator : IDummyDataGenerator
+    {
+
+    }
+}
