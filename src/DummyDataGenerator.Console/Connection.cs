f591acb55d4c68661e5667b9b43cfd0aabff0825
Bernhard Maier <mail@bernhard-maier.net>
2021-10-14 08:10:00 +0200

    feat(dummyDataGenerator): implemented classes and first logic

---
+++ src/DummyDataGenerator.Console/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace DummyDataGeneratorConsole
+{
+    public record Connection
+    {
+        public Customer Customer { get; }
+        public Vehicle Vehicle { get; }
+
+        public Connection(Customer customer, Vehicle vehicle)
+        {
+            Customer = customer;
+            Vehicle = vehicle;
+        }
+
+        public static IEnumerable<Connection> CreateMany(
+            int count,
+            IEnumerable<Customer> customers,
+            IEnumerable<Vehicle> vehicles,
+            IDummyDataGenerator ddg)
+        {
+            var connections = new List<Connection>();
+            
+            for (var i = 0; i < count; i++)
+            {
+                //TODO create random connection. But be aware: Customer can have many cars, but a car can only have one customer
+            }
+
+            return connections;
+        }
+    }
+}
---
+++ src/DummyDataGenerator.Console/SqlHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DummyDataGeneratorConsole
+{
+    public static class SqlHandler
+    {
+        public static string CreateScript(
+            IEnumerable<Customer> customers,
+            IEnumerable<Vehicle> vehicles,
+            IEnumerable<Connection> connections)
+        {
+            return "";
+        }
+
+        public static void SaveScript(string path, string content)
+        {
+            
+        }
+    }
+}
