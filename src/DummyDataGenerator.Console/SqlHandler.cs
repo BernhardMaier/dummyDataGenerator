89872e320eeab732ce1398689bba25decf4519d0
Bernhard Maier <mail@bernhard-maier.net>
2021-10-19 07:32:07 +0200

    feat(dummyDataGenerator): improve char generation and script output

--- src/DummyDataGenerator.Console/SqlHandler.cs
+++ src/DummyDataGenerator.Console/SqlHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace DummyDataGeneratorConsole
 {
@@ -12,15 +14,24 @@ namespace DummyDataGeneratorConsole
         {
             var script = new List<string>();
 
+            customers = customers.ToList();
+            script.Add($"-- CUSTOMERS ({customers.Count()})");
             foreach (var customer in customers)
                 script.Add(customer.AsInsertScript());
-            
+            script.Add(string.Empty);
+
+            vehicles = vehicles.ToList();
+            script.Add($"-- VEHICLES ({vehicles.Count()})");
             foreach (var vehicle in vehicles)
                 script.Add(vehicle.AsInsertScript());
-            
+            script.Add(string.Empty);
+
+            connections = connections.ToList();
+            script.Add($"-- CONNECTIONS ({connections.Count()})");
             foreach (var connection in connections)
                 script.Add(connection.AsInsertScript());
-            
+            script.Add(string.Empty);
+
             return script.ToArray();
         }
 
