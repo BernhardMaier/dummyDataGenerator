4b76f41f6fce1fa12601da6194fcbf333616e7fd
Bernhard Maier <mail@bernhard-maier.net>
2021-10-19 09:42:01 +0200

    feat(dummyDataGenerator): add logic to create connections

--- src/DummyDataGenerator.Console/Connection.cs
+++ src/DummyDataGenerator.Console/Connection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace DummyDataGeneratorConsole
@@ -9,23 +11,32 @@ namespace DummyDataGeneratorConsole
         public Customer Customer { get; }
         public Vehicle Vehicle { get; }
 
-        public Connection(Customer customer, Vehicle vehicle, IDummyDataGenerator ddg)
+        public Connection(Customer customer, Vehicle vehicle)
         {
             Customer = customer;
             Vehicle = vehicle;
         }
 
         public static IEnumerable<Connection> CreateMany(
-            int count,
-            IEnumerable<Customer> customers,
-            IEnumerable<Vehicle> vehicles,
-            IDummyDataGenerator ddg)
+            Customer[] customers,
+            Vehicle[] vehicles)
         {
             var connections = new List<Connection>();
-            
-            for (var i = 0; i < count; i++)
+            for (var i = 0; i < customers.Length; i++)
             {
-                //TODO create random connection. But be aware: Customer can have many cars, but a car can only have one customer
+                switch ((i+4)%4)
+                {
+                    case 0:
+                    case 1:
+                        continue;
+                    case 2:
+                        connections.Add(new Connection(customers[i], vehicles[i-1]));
+                        break;
+                    case 3:
+                        connections.Add(new Connection(customers[i], vehicles[i-1]));
+                        connections.Add(new Connection(customers[i], vehicles[i]));
+                        break;
+                }
             }
 
             return connections;
@@ -34,7 +45,7 @@ namespace DummyDataGeneratorConsole
         public string AsInsertScript()
         {
             var sb = new StringBuilder();
-            
+
             sb.Append($"INSERT INTO [dbo].[Connections] ");
             sb.Append($"[CustomerId],");
             sb.Append($"[VehicleId])");
