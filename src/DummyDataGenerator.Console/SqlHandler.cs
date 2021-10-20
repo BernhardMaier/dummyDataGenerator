1404ed54c1a0d6a475f4b24820449556412d8d28
Bernhard Maier <mail@bernhard-maier.net>
2021-10-20 06:59:53 +0200

    feat(dummyDataGenerator): add comment header to sql script

--- src/DummyDataGenerator.Console/SqlHandler.cs
+++ src/DummyDataGenerator.Console/SqlHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -11,13 +12,29 @@ namespace DummyDataGenerator.Console
       IEnumerable<Vehicle> vehicles,
       IEnumerable<Connection> connections)
     {
+      customers = customers.ToList();
+      vehicles = vehicles.ToList();
+      connections = connections.ToList();
+
       var script = new List<string>();
 
+      AddCommentHeader(script, customers.Count(), vehicles.Count(), connections.Count());
       AddSqlEntities(script, customers, nameof(customers).ToUpper());
       AddSqlEntities(script, vehicles, nameof(vehicles).ToUpper());
       AddSqlEntities(script, connections, nameof(connections).ToUpper());
 
       return script.ToArray();
+    }
+
+    private static void AddCommentHeader(ICollection<string> script, int customers, int vehicles, int connections)
+    {
+      script.Add("-- ############");
+      script.Add($"-- # Creation date: {DateTime.Now}");
+      script.Add($"-- # Customers: {customers}");
+      script.Add($"-- # Vehicles: {vehicles}");
+      script.Add($"-- # Connections: {connections}");
+      script.Add("-- ############");
+      script.Add(string.Empty);
     }
 
     private static void AddSqlEntities(ICollection<string> script, IEnumerable<ISqlEntity> list, string name)
