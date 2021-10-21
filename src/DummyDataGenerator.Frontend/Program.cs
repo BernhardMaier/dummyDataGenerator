b18e5cd9c98b098aee2b30349f970dac9d2419e9
Bernhard Maier <mail@bernhard-maier.net>
2021-10-21 07:29:49 +0200

    feat(dummyDataGenerator): make SqlHandler non-static

--- src/DummyDataGenerator.Frontend/Program.cs
+++ src/DummyDataGenerator.Frontend/Program.cs
@@ -15,16 +15,18 @@ namespace DummyDataGenerator.Frontend
       RequestInput("Please enter a seed number (integer):", out var seed);
       RequestInput("Please enter how many customers/vehicles to generate (integer):", out var count);
 
-      var path = Path.Combine(Directory.GetCurrentDirectory(), $"{filename}_{count}.{extension}");
+      var filePath = Path.Combine(Directory.GetCurrentDirectory(), $"{filename}_{count}.{extension}");
+      var sqlCreator = new SqlHandler(filePath);
       var ddg = new DummyDataGenerator(seed);
+      
       var customers = Customer.CreateMany(count, ddg).ToList();
       var vehicles = Vehicle.CreateMany(count, ddg).ToList();
       var connections = Connection.CreateMany(customers.ToArray(), vehicles.ToArray()).ToList();
 
-      var content = SqlHandler.CreateScript(customers, vehicles, connections);
-      SqlHandler.SaveScript(path, content);
+      sqlCreator.CreateScript(customers, vehicles, connections);
+      sqlCreator.SaveScript();
 
-      PrintResult(ddg.Seed, customers.Count, vehicles.Count, connections.Count, path);
+      PrintResult(ddg.Seed, customers.Count, vehicles.Count, connections.Count, sqlCreator.FilePath);
     }
 
     private static void RequestInput(string message, out int inputVariable)
